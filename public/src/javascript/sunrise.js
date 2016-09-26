5a86684bccb89cf9da46491757850279962b1ade
robert-marriott <robertmarriot@C02S222CFVH8.local>
2016-09-26 12:47:31 -0400

    added sunrise skeleton

---
+++ public/src/javascript/sunrise.js
@@ -0,0 +1,7 @@
+/**
+ * Created by robertmarriot on 9/26/16.
+ */
+/* sunrise.js is a library for PWM control of LED's on Raspbery Pi
+GPIO pins. It is meant to be app agnostic.
+ */
+console.log("sunrise API Loaded!");
